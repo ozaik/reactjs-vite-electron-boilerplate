1b1c916f962c52e6973788a8581d8f7961174a6c
Mathieu Mesquita <mathieumesquita@MacBook-Pro-de-Mathieu-2.local>
2023-03-28 20:58:38 +0200

    Bug fix

--- src/main/index.js
+++ src/main/index.js
@@ -2,9 +2,6 @@ import { app, shell, BrowserWindow } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
-const test = require('../../resources/test/test')
-
-test()
 
 function createWindow() {
   // Create the browser window.
